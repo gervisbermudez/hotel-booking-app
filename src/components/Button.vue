c3261701d954cb7145b31c2a0c22e4095f96e8b3
Gervis Bermudez <gervis.bermudez@tsoftglobal.com>
2023-05-26 20:52:42 -0300

    fix: button component, change color behavior

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -4,24 +4,9 @@
     </button>
 </template>
 <script lang="ts">
-
 export default {
-    props: {
-        type: {
-            default: "primary",
-            validator(value: string) {
-                return ['primary', 'secundary'].includes(value);
-            }
-        }
-    },
-    created() {
-        // props are exposed on `this`
-        console.log(this.type)
 
-    }
 }
-
-
 </script>
 <style lang="scss" scoped>
 @import "../assets/styles/vars.scss";
@@ -31,16 +16,40 @@ button {
     padding: 0 1.5rem;
     border-radius: 4px;
     outline: none;
-    background-color: $primary;
-    color: $white;
-    border: 1px solid $primary;
+    background-color: $white;
+    color: $black;
+    border: 1px solid $white;
     cursor: pointer;
     // Transición para el hover
     transition: background-color 0.3s, color 0.3s;
 
     &:hover {
-        background-color: $white;
-        color: $primary;
+        background-color: darken($white, 20%);
+        border-color: darken($white, 20%);
+    }
+
+    &.primary {
+        background-color: $primary;
+        color: $black;
+        border-color: $primary;
+
+        &:hover {
+            background-color: darken($primary, 20%);
+            border-color: darken($primary, 20%);
+        }
+
+    }
+
+    &.secondary {
+        background-color: $secondary;
+        color: $white;
+        border-color: $secondary;
+
+        &:hover {
+            background-color: lighten($secondary, 20%);
+            border-color: lighten($secondary, 20%);
+        }
+
     }
 }
 </style>
